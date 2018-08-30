a913f422c5a3ed5da4ba3e79d9ad059c9e7deb3f
virus <yystopf@163.com>
2018-08-30 15:13:45 +0800

    fix

--- language/example/1-12.py
+++ language/example/1-12.py
@@ -5,3 +5,23 @@
 # @File : 1-12.py 
 # @Desp : python
 
+
+# 三整数排序
+a = int(input("请输入第一个数"))
+b = int(input("请输入第二个数"))
+c = int(input("请输入第三个数"))
+
+if a > b:
+    t = a
+    a = b
+    b = t
+if a > c:
+    t = a
+    a = c
+    c = t
+if b > c:
+    t = b
+    b = c
+    c = t
+
+print("%d < %d < %d"%(a,b,c))
