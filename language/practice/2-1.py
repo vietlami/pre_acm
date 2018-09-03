7ee0ea22bf3a71f97799427babd3cd20dcb35948
virus <yystopf@163.com>
2018-09-03 15:55:19 +0800

    水仙花数

---
+++ language/practice/2-1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2018/9/3 15:47 
+# @Author : virus 
+# @File : 2-1.py 
+# @Desp : python
+
+n = 100
+
+while n < 1000:
+    # print(n)
+    if n == int(n/100)*int(n/100)*int(n/100) + int(n%100/10)*int(n%100/10)*int(n%100/10) + int(n%10)*int(n%10)*int(n%10):
+        print(n)
+    n +=1
