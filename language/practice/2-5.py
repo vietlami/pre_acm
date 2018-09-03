ee5f8155c776fe0b98676a1dd2aedd030dda81bc
virus <yystopf@163.com>
2018-09-03 17:22:40 +0800

    分数化小数

---
+++ language/practice/2-5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2018/9/3 17:16 
+# @Author : virus 
+# @File : 2-5.py 
+# @Desp : python
+
+# 分数化小数
+a = int(input("请输入第一个正整数\n"))
+b = int(input("请输入第二个正整数\n"))
+c = int(input("请输入第三个正整数\n"))
+
+f_f = round(float(a)/b,c)
+
+print("%s"%f_f)
+print("a=b=c=0")
